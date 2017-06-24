49965d3f86e5c1394732d272c1ef4d8866569146
saingrampeters <samuelingrampeters@hotmail.com>
2017-06-24 20:54:00 +0100

    testtttt

---
+++ testindexpage.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by SPBHGAMES.
+ * User: Samuel
+ * Date: 22/04/2017
+ * Time: 05:13
+ */
+
+// echo "<div>YOOOOOOOOOOOOOOOOOOOOOO</div>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="styles/style.css">
+	<meta charset="UTF-8">
+	<title>Home</title>
+</head>
+<body>
+
+	<header>
+		<nav>
+			<h1>My Page</h1>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="hello.php">Blog</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Links</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<div>
+		<div class="col">col1</div>
+		<div class="col">col2</div>
+		<div class="col">col3</div>
+	</div>
+
+	<footer>c2017 My Site</footer>
+</body>
+</html>
